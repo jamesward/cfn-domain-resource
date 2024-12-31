009a003ff07bfad8350044260a5d35d019bdf986
James Ward <james@jamesward.com>
2024-12-31 05:37:31 -0700

    init

---
+++ index.py
@@ -0,0 +1,301 @@
+from __future__ import print_function
+
+from abc import abstractmethod, ABC
+from dataclasses import dataclass
+
+from crhelper import CfnResource
+import logging
+import boto3
+
+from typing import Optional, List
+
+logger = logging.getLogger(__name__)
+
+helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120, ssl_verify=None)
+
+try:
+    # domain_manager = DomainManagerLive()
+    pass
+except Exception as e:
+    helper.init_failure(e)
+
+
+class DomainManager(ABC):
+    @abstractmethod
+    def list_domains(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def list_operations(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_domain_detail(self, domain_name):
+        pass
+
+    @abstractmethod
+    def get_operation_detail(self, operation_id):
+        pass
+
+    def get_domain(self, domain_name):
+        domains_response = self.list_domains()
+
+        for domain in domains_response['Domains']:
+            if domain['DomainName'] == domain_name:
+                return self.get_domain_detail(domain_name)
+
+        operations_response = self.list_operations()
+
+        for operation in operations_response['Operations']:
+            if (operation['Status'] == 'IN_PROGRESS' and
+                operation['Type'] == 'DOMAIN_TRANSFER_IN' and
+                operation['DomainName'] == domain_name):
+                return self.get_operation_detail(operation['OperationId'])
+
+        return None
+
+    @abstractmethod
+    def check_domain_availability(self, domain_name):
+        pass
+
+    @abstractmethod
+    def register_domain(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def update_domain_nameservers(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def check_domain_transferability(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def transfer_domain(self, **kwargs):
+        pass
+
+class DomainManagerLive(DomainManager):
+
+    def __init__(self):
+        self.client = boto3.client('route53domains')
+
+    def list_domains(self, **kwargs):
+        self.client.list_domains(**kwargs)
+
+    def list_operations(self, **kwargs):
+        self.client.list_operations(**kwargs)
+
+    def get_domain_detail(self, domain_name):
+        self.client.get_domain_detail(DomainName = domain_name)
+
+    def get_operation_detail(self, operation_id):
+        self.client.get_operation_detail(OperationId = operation_id)
+
+    def check_domain_availability(self, domain_name):
+        return self.client.check_domain_availability(DomainName = domain_name)
+
+    def register_domain(self, **kwargs):
+        return self.client.register_domain(**kwargs)
+
+    def update_domain_nameservers(self, **kwargs):
+        return self.client.update_domain_nameservers(**kwargs)
+
+    def check_domain_transferability(self, **kwargs):
+        return self.client.check_domain_transferability(**kwargs)
+
+    def transfer_domain(self, **kwargs):
+        return self.client.transfer_domain(**kwargs)
+
+@dataclass
+class Contact:
+    first_name: str
+    last_name: str
+    contact_type: str
+    address_line_1: str
+    city: str
+    state: str
+    country_code: str
+    zip_code: str
+    phone_number: str
+    email: str
+
+
+@dataclass
+class DomainEvent:
+    domain_name: str
+    contact: Contact
+    auto_renew: bool
+    name_servers: Optional[List[str]]
+
+
+def parse_event(event):
+    return DomainEvent(
+        domain_name=event['ResourceProperties']['DomainName'],
+        contact=Contact(
+            first_name=event['ResourceProperties']['Contact']['firstName'],
+            last_name=event['ResourceProperties']['Contact']['lastName'],
+            contact_type=event['ResourceProperties']['Contact'].get('type'),
+            address_line_1=event['ResourceProperties']['Contact']['addressLine1'],
+            city=event['ResourceProperties']['Contact']['city'],
+            state=event['ResourceProperties']['Contact']['state'],
+            country_code=event['ResourceProperties']['Contact']['countryCode'],
+            zip_code=event['ResourceProperties']['Contact']['zipCode'],
+            phone_number=event['ResourceProperties']['Contact']['phoneNumber'],
+            email=event['ResourceProperties']['Contact']['email']
+        ),
+        auto_renew=event['ResourceProperties']['AutoRenew'],
+        name_servers=event['ResourceProperties'].get('NameServers', [])
+    )
+
+def domain_manager(context) -> DomainManager:
+    return context['domain_manager']
+
+@helper.create
+def create(event, context):
+    logger.info("Got Create")
+    domain_event = parse_event(event)
+    domain = domain_manager(context).get_domain(domain_event.domain_name)
+
+    if domain is None:
+        transfer_auth_code = event['ResourceProperties'].get('TransferAuthCode')
+
+        if transfer_auth_code is None:
+            availability = domain_manager(context).check_domain_availability(domain_event.domain_name)
+
+            if availability['Availability'] == 'AVAILABLE':
+                domain_manager(context).register_domain(
+                    DomainName = domain_event.domain_name,
+                    DurationInYears = 1,
+                    AutoRenew = domain_event.auto_renew,
+                    AdminContact = domain_event.contact,
+                    RegistrantContact = domain_event.contact,
+                    TechContact = domain_event.contact,
+                    PrivacyProtectAdminContact = True,
+                    PrivacyProtectRegistrantContact = True,
+                    PrivacyProtectTechContact = True
+                )
+
+                if domain_event.name_servers:
+                    domain_manager(context).update_domain_nameservers(
+                        DomainName = domain_event.domain_name,
+                        Nameservers = [{'Name': ns} for ns in domain_event.name_servers]
+                    )
+            else:
+                raise Exception(f"Domain {domain_event.domain_name} is not available")
+        else:
+            transferability = domain_manager(context).check_domain_transferability(
+                DomainName = domain_event.domain_name,
+                AuthCode = transfer_auth_code
+            )
+
+            if transferability['Transferability']['Transferable'] == 'TRANSFERABLE':
+                params = {
+                    'DomainName': domain_event.domain_name,
+                    'AuthCode': transfer_auth_code,
+                    'DurationInYears': 1,
+                    'AutoRenew': domain_event.auto_renew,
+                    'AdminContact': domain_event.contact,
+                    'RegistrantContact': domain_event.contact,
+                    'TechContact': domain_event.contact,
+                    'PrivacyProtectAdminContact': True,
+                    'PrivacyProtectRegistrantContact': True,
+                    'PrivacyProtectTechContact': True
+                }
+
+                if domain_event.name_servers:
+                    params['Nameservers'] = [{'Name': ns} for ns in domain_event.name_servers]
+
+                domain_manager(context).transfer_domain(**params)
+            else:
+                raise Exception(f"Domain {domain_event.domain_name} is not transferable")
+
+    return domain_event.domain_name
+
+
+@helper.update
+def update(event, context):
+    logger.info("Got Update")
+    domain_event = parse_event(event)
+    domain = domain_manager(context).get_domain(domain_event.domain_name)
+
+    if domain is None:
+        raise Exception(f"Domain {domain_event.domain_name} does not exist")
+    else:
+        # todo: update domain
+        pass
+
+    return domain_event.domain_name
+
+    # domain_details = client.get_domain_detail(
+    #     DomainName = domain_name(event)
+    # )
+    #
+    # status = domain_details.get('StatusList', [])
+    # pending_transfer = any('PENDING_TRANSFER' in s for s in status)
+    #
+    # def contacts_are_different(new_contact, old_contact):
+    #
+    #     print(f"New: {str(new_contact)}")
+    #     print(f"Old: {str(old_contact)}")
+    #
+    #     fields_to_compare = [
+    #         'FirstName',
+    #         'LastName',
+    #         'ContactType',
+    #         'OrganizationName',
+    #         'AddressLine1',
+    #         'AddressLine2',
+    #         'City',
+    #         'State',
+    #         'CountryCode',
+    #         'ZipCode',
+    #         'PhoneNumber',
+    #         'Email'
+    #     ]
+    #
+    #     for field in fields_to_compare:
+    #         if old_contact.get(field) != new_contact.get(field):
+    #             return True
+    #
+    #     return False
+    #
+    # if not pending_transfer:
+    #     updated_admin_contact = contacts_are_different(contact(event), domain_details.get('AdminContact', {}))
+    #     updated_registrant_contact = contacts_are_different(contact(event), domain_details.get('RegistrantContact', {}))
+    #     updated_tech_contact = contacts_are_different(contact(event), domain_details.get('TechContact', {}))
+    #
+    #     if updated_admin_contact or updated_registrant_contact or updated_tech_contact:
+    #         client.update_domain_contact(
+    #             DomainName = domain_name(event),
+    #             AdminContact = contact(event),
+    #             RegistrantContact = contact(event),
+    #             TechContact = contact(event)
+    #         )
+    #
+    #     # todo: update autoRenew
+    #
+    #     # todo: if updated
+    #     if name_servers(event):
+    #         client.update_domain_nameservers(
+    #             DomainName = domain_name(event),
+    #             Nameservers = name_servers(event)
+    #         )
+
+
+@helper.delete
+def delete(event, context):
+    logger.info("Got Delete")
+
+    # client.get_domain_detail(
+    #     DomainName = domain_name
+    # )
+
+    # How do we handle Delete? What about rollbacks? What about moving from one CFN to another? Maybe a field that confirms we actually want to delete it?
+    # client.delete_domain(
+    #  DomainName = domain_name
+    # )
+
+
+def handler(event, context):
+    context.domain_manager = DomainManagerLive()
+    helper(event, context)
---
+++ test_all.py
@@ -0,0 +1,74 @@
+import pytest
+import index
+from index import DomainManager
+
+
+class DomainManagerFake(DomainManager):
+    events = []
+
+    def list_operations(self, **kwargs):
+        return {}
+
+    def get_domain_detail(self, domain_name):
+        return {
+            'DomainName': domain_name
+        }
+
+    def get_operation_detail(self, operation_id):
+        pass
+
+    def list_domains(self, **kwargs):
+        return {
+            'Domains': [
+                {
+                    'DomainName': "foo.com"
+                }
+            ]
+        }
+
+    def check_domain_availability(self, **kwargs):
+        pass
+
+    def register_domain(self, **kwargs):
+        self.events.append("register_domain")
+
+    def update_domain_nameservers(self, **kwargs):
+        pass
+
+    def check_domain_transferability(self, **kwargs):
+        pass
+
+    def transfer_domain(self, **kwargs):
+        self.events.append("transfer_domain")
+
+def context():
+    return {
+        'domain_manager': DomainManagerFake()
+    }
+
+def test_exists():
+
+    event = {
+        'ResourceProperties': {
+            'DomainName': "foo.com",
+            'Contact': {
+                'firstName': "Joe",
+                'lastName': "Bob",
+                'type': "PERSON",
+                'phoneNumber': "+1.3035551212",
+                'email': "joe@bob.com",
+                'addressLine1': "PO Box 123",
+                'city': "Nowhere",
+                'state': "CA",
+                'countryCode': "US",
+                'zipCode': "91222"
+            },
+            'AutoRenew':  True
+        }
+    }
+
+    my_context = context()
+
+    response = index.create(event, my_context)
+    assert response == "foo.com"
+    assert len(my_context['domain_manager'].events) == 0
